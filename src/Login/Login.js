9678781989c6a3e569edcc876725c544d6291f69
My Name <cpgsom819@gmail.com>
2023-03-22 04:32:27 +0800

    starting

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import img from '../assets/images/login/login.svg';
+import { AuthContext } from './../Context/AutProvider';
 
 const Login = () => {
 
+
+    const {login} = useContext(AuthContext)
+
+
     const handleLogin = event =>{
         event.preventDefault();
+
+        const form = event.target;
+
+        const email = form.email.value;
+        const password = form.password.value;
+
+        login(email,password)
+        .then(result => {
+
+            const user = result.user;
+
+            console.log(user)
+        })
+
+        .then(error => console.log(error))
+        
+
     }
 
     return (

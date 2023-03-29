e480ceba59cf33473af450835a2bba9186d2a051
My Name <cpgsom819@gmail.com>
2023-03-30 00:31:17 +0800

    jwt done

--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -3,14 +3,30 @@ import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 import OrderRow from './OrderRow';
 
 const Orders = () => {
-    const { user } = useContext(AuthContext);
+    const { user, logOut } = useContext(AuthContext);
     const [orders, setOrders] = useState([])
 
     useEffect(() => {
-        fetch(`https://car-server-lac.vercel.app/orders?email=${user?.email}`)
-            .then(res => res.json())
+        fetch(`https://car-server-lac.vercel.app/orders?email=${user?.email}`, {
+
+         headers : {
+
+            authorization : `Bearer ${localStorage.getItem('token')}`
+
+         }     
+
+
+        })
+            .then(res =>   {
+
+                if(res.status === 401 || res.status === 403) {
+
+                  logOut()
+                }
+               return  res.json()
+            })
             .then(data => setOrders(data))
-    }, [user?.email])
+    }, [user?.email, logOut])
 
     const handleDelete = id =>{
         const proceed = window.confirm('Are you sure, you want to cancel this order');
